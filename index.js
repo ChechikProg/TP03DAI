eb0084354bc08e94126a8aee596d3e1587a50101
JUAN SEBASTIAN CARUS <48802882@ort>
2026-03-27 12:12:50 -0300

    ejercicio 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //ejercicio 1
 import fs from 'fs';
-import { json } from 'stream/consumers';
+import dayjs from 'dayjs';
 const contenido = fs.readFileSync('productos.json','utf-8');
 console.log('Contenido del archivo',contenido);
 
@@ -16,4 +16,15 @@ fs.writeFileSync("productos.json", JSON.stringify(productos, null, 2), "utf-8");
 console.log("Producto guardado exitosamente");
 
 }
-agregarProducto("Mouse", 12000);
+//agregarProducto("Mouse", 12000);
+//ejercicio 3
+
+
+function mostrarFechaYHora() {
+ const ahora = dayjs();
+const fecha = ahora.format('DD/MM/YYYY');
+const hora = ahora.format('HH:mm');
+console.log("Fecha actual:", fecha);
+console.log("Hora actual:", hora);
+}
+mostrarFechaYHora();
